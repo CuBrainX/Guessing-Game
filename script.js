bf055f13ac05cd2bf149351e1f826f9483c55f2b
Nasir Ali <Nasir7dawood@gmail.com>
2025-08-19 14:23:04 +0400

    fix

--- script.js
+++ script.js
@@ -1,61 +1,54 @@
-var counter = 0 
-var randomNumber = Math.random()*100
-var name = ""
-var GuessedNumber = 0
+var counter = 0;
+var randomNumber = Math.round(Math.random() * 100);
+var name = "";
+var GuessedNumber = 0;
+
+function Submit() {
+  let diff = Math.abs(GuessedNumber - randomNumber);
+  let percent = diff;
+
+  let veryClose = 5;
+  let close = 15;
+  let far = 25;
+  let veryFar = 35;
+  let closeness = "";
+  name = document.getElementById("name").value;
+
+  GuessedNumber = parseInt(document.getElementById("GuessedNumber").value);
+  if (percent <= veryClose) {
+    closeness = "Very Close";
+  } else if (percent <= close) {
+    closeness = "Near";
+  } else if (percent <= far) {
+    closeness = "Far";
+  } else {
+    closeness = "Very far";
+  }
+
+  let hint = "";
+
+  console.log(hint, GuessedNumber < randomNumber, GuessedNumber, randomNumber);
+  
+  if (GuessedNumber < randomNumber) {
+    hint = "Try Higher ↑";
+  } else {
+    hint = "Try Lower ↓";
+  }
 
 
 
-function Submit(){
+  if (GuessedNumber === randomNumber) {
+    document.getElementById("returnMessage").innerText = "Correct Answer";
+  } else {
+    document.getElementById("returnMessage").innerText =
+      "Wrong Answer " + name + ", " + closeness + " " + hint;
+  }
 
-    let diff = Math.abs(GuessedNumber - randomNumber)
-    let percent = (diff * 100) 
+  counter += 1;
 
-    let veryClose = 5
-    let close = 15
-    let far = 25
-    let veryFar = 35
-    let closeness = ""
+  document.getElementById("counter").innerText = counter;
 
-     if (percent <= veryClose) {
-            
-            closeness = "Very Close"
-        }else if (percent <= close) {
-            closeness = "Near"
-        }else if (percent <= far) {
-            closeness = "Far"
-        }else{
-            closeness = "Very far"
-        }
-
-
-        let hint = ""
-
-        if(GuessedNumber < randomNumber){
-            hint = "Try Higher ↑"
-        }else{
-            hint = "Try Lower ↓"
-        }
-
-    name = document.getElementById("name").value
-
-    GuessedNumber = parseInt(document.getElementById("GuessedNumber").value)
-
-    if (GuessedNumber === randomNumber){
-        document.getElementById("returnMessage").innerText = "Correct Answer"
-    }else {
-         document.getElementById("returnMessage").innerText = "Wrong Answer "  + name + ", " + closeness + " " + hint
-    }
-
-
-
-    counter += 1
-
-
-    document.getElementById("counter").innerText = counter
-
-    if (isNaN(GuessedNumber)){
-        alert("Please write a number")
-    }
-
-    
+  if (isNaN(GuessedNumber)) {
+    alert("Please write a number");
+  }
 }
